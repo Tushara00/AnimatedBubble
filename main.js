73996ddd7e51f5be966419cbc7599a2f61bcc62a
Tushara00 <ahnafmon6@gmail.com>
2023-05-16 13:05:50 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+(function () {
+    function createElement() {
+        const container = document.querySelector(".container");
+        const element = document.createElement("span");
+        const size = Math.random() * 60;
+        element.style.width = 20 + size + "px";
+        element.style.height = 20 + size + "px";
+      element.style.left = Math.random() * innerWidth + "px";      
+        container.appendChild(element);
+        setTimeout(() => {
+            element.remove();
+        }, 4000);
+    }
+
+    setInterval(createElement, 50);
+})();
